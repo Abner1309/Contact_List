4cbaf192be4c6c19c80c97b39f9ef07399f202b4
AbnerBC <159132346+Abner1309@users.noreply.github.com>
2025-10-27 16:13:57 -0300

    feat: Create CreateDB class to initialize contacts table

---
+++ src/main/java/com/agenda/controller/CreateDB.java
@@ -0,0 +1,26 @@
+package com.agenda.controller;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateDB {
+    public static void createRelation() throws RuntimeException {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS contacts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                phone TEXT,
+                email TEXT
+                );
+                """;
+
+        try (Connection conn = ConnectDB.connect();
+        Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
